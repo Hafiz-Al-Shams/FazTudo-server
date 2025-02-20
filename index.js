1e01fdc4f88c181fadc272c9fd8ff2a08cfe1ffd
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-02-20 19:30:10 +0600

    added mongodb successfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,110 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            // "https://cure-medix.web.app",
+            // "https://cure-medix.firebaseapp.com",
+            // "https://cure-medix-by-hafiz-al-shams1917.netlify.app",
+        ],
+        credentials: true,
+    })
+);
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkfjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+        const userCollection = client.db("fazTudoDB").collection("users");
+
+
+
+
+
+        // users APIs
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+
+
+        app.post('/users', async (req, res) => {
+            console.log('click successful');
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 
 
